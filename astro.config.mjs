b9af5fae3f196b5337bbf88319fa619ec0d1731b
cami98735264 <cr7montealegre2010@gmail.com>
2024-07-11 18:09:10 -0500

    Changed to netlify provider

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,13 @@
 import { defineConfig } from 'astro/config';
 import icon from "astro-icon";
-
 import node from "@astrojs/node";
+
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
-    site: "https://cami98735264.github.io/",
-    base: "portfolio-website",
-    integrations: [icon()],
-    output: "server",
-    adapter: node({
-        mode: "standalone"
-    })
+  site: "https://cami98735264.github.io/",
+  integrations: [icon()],
+  output: "server",
+  adapter: netlify()
 });
