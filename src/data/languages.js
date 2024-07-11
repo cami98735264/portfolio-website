30ffa339b04b30c772ccf516e22f4107d69e2b49
cami98735264 <cr7montealegre2010@gmail.com>
2024-07-11 16:02:47 -0500

    Main page

---
+++ src/data/languages.js
@@ -0,0 +1,55 @@
+
+const languages = {
+    es: {
+        aboutMe: "¡Hola! 👋,  Mi nombre es Cristián. Soy un desarrollador Full Stack especializado en backend y con amplios conocimientos en JavaScript, ReactJS y ExpressJS. Me destaco en programación avanzada, gestión de bases de datos y diseño de experiencias de usuario (UI/UX).",
+        languagesList: [{
+            name: "Español",
+            value: "es"
+        }, {
+            name: "Inglés",
+            value: "en"
+        
+        }],
+        navbarItems: [
+        {
+            name: "Sobre mí",
+            redirectUrl: "#sobre-mi"
+        },
+        {
+            name: "Proyectos",
+            redirectUrl: "#proyectos"
+        },
+        {
+            name: "Contacto",
+            redirectUrl: "#contacto"
+        }]
+    },
+    en: {
+        aboutMe: "Hello! 👋, My name is Cristián. I'm a Full Stack developer specializing in backend and with extensive knowledge in JavaScript, ReactJS, and ExpressJS. I excel in advanced programming, database management, and user experience design (UI/UX).",
+        languagesList: [{
+            name: "Spanish",
+            value: "es"
+        }, {
+            name: "English",
+            value: "en"
+        
+        }],
+        navbarItems: [
+        {
+            name: "About me",
+            redirectUrl: "#about-me"
+        },
+        {
+            name: "Projects",
+            redirectUrl: "#projects"
+        },
+        {
+            name: "Contact",
+            redirectUrl: "#contact"
+        }
+    ],
+    }
+};
+
+
+export default languages;
