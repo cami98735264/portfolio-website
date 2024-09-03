9ae3c80945226374c8c9081430a3383128ebbc2a
cami98735264 <cr7montealegre2010@gmail.com>
2024-09-03 01:21:39 -0500

    Add languages features

---
+++ src/i18n/ui.ts
@@ -0,0 +1,44 @@
+export const languages = {
+    en: 'English',
+    es: 'Español',
+  };
+
+export const showDefaultLang = true;
+
+export const defaultLang = 'es';
+export const routes = {
+    es: {
+        'projects': 'proyectos',
+        'skills': 'habilidades',
+        'experience': 'experiencia',
+    },
+  }
+
+export const ui = {
+    en: {
+        'nav.aboutMe': 'About me',
+        'nav.projects': 'Projects',
+        'nav.skills': 'Skills',
+        'nav.experience': 'Experience',
+        'aboutMe.title': 'Hello! 👋 My name is Cristián, I am a Full Stack developer',
+        'aboutMe.description': 'I\'m a Full Stack developer specializing in backend and with extensive knowledge in JavaScript, ReactJS, and ExpressJS. I excel in advanced programming, database management, and user experience design (UI/UX).',
+        'aboutMe.contact': 'Contact me',
+        'aboutMe.resume': 'Download CV',
+        'emailContact.subject': 'Hi! I\'d like to talk about your services',
+        'emailContact.body': 'Hi Cristián, I found your portfolio and I would like to talk to you about your services. When can we meet?',
+        'whatsappContact.text': 'Hi Cristián, I found your portfolio and I would like to talk to you about your services. When can we meet?',
+    },
+    es: {
+        'nav.aboutMe': 'Sobre mi',
+        'nav.projects': 'Proyectos',
+        'nav.skills': 'Habilidades',
+        'nav.experience': 'Experiencia',
+        'aboutMe.title': 'Hola! 👋 Mi nombre es Cristián, soy un desarrollador Full Stack',
+        'aboutMe.description': 'Soy un desarrollador Full Stack especializado en backend y con amplios conocimientos en JavaScript, ReactJS y ExpressJS. Destaco en programación avanzada, gestión de bases de datos y diseño de experiencia de usuario (UI/UX).',
+        'aboutMe.contact': 'Contáctame',
+        'aboutMe.resume': 'Descargar CV',
+        'emailContact.subject': 'Hola! Me gustaría hablar sobre tus servicios',
+        'emailContact.body': 'Hola Cristián, encontré tu portafolio y me gustaría hablar contigo sobre tus servicios. ¿Cuándo podemos reunirnos?',
+        'whatsappContact.text': 'Hola Cristián, encontré tu portafolio y me gustaría hablar contigo sobre tus servicios. ¿Cuándo podemos reunirnos?',
+    },
+} as const;
